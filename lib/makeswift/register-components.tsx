0fb0eb88b6ffe02728b676c7c47687be21fdb619
Maximilian Schön <Maximilianschon@outlook.com>
2022-08-05 21:44:36 +0200

    first commit

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -1,16 +1,46 @@
-import { Style } from '@makeswift/runtime/controls'
+import { Color, Style, TextInput } from '@makeswift/runtime/controls'
 import { ReactRuntime } from '@makeswift/runtime/react'
+import useSWR from 'swr'
 
 // Register your components here!
 
-function HelloWorld(props: { className?: string }) {
-  return <p {...props}>Hello, world!</p>
+function DataText({dataURL, placeholder, color, ...props}: { placeholder?: string, color: string, dataURL?: string, className?: string }) {
+  const { data, error } = useSWR(dataURL)
+  return <p style={{color}} {...props}>{data ? data : (placeholder ?? dataURL ?? 'Data-URL & placeholder missing')}</p>
 }
 
-ReactRuntime.registerComponent(HelloWorld, {
-  type: 'hello-world',
-  label: 'Hello, world!',
+ReactRuntime.registerComponent(DataText, {
+  type: 'text',
+  label: 'Data Text',
   props: {
     className: Style({ properties: Style.All }),
+    dataURL: TextInput({
+      label: "Data-URI"
+    }),
+    placeholder: TextInput({
+      label: "Placeholder"
+    }),
+    color: Color({ label: 'Text color', defaultValue: 'white' }),
   },
 })
+
+
+function DataImage({dataURL, alt, ...props}: { alt?: string, dataURL?: string, className?: string }) {
+  const { data, error } = useSWR(dataURL)
+  return <img src={data} alt={alt} {...props} />
+}
+
+ReactRuntime.registerComponent(DataImage, {
+  type: 'image',
+  label: 'Data Image',
+  props: {
+    className: Style({ properties: Style.All }),
+    dataURL: TextInput({
+      label: "Data-URI"
+    }),
+    alt: TextInput({
+      label: "Alternative"
+    }),
+  },
+})
+
---
+++ pages/[[...path]].tsx
@@ -0,0 +1,105 @@
+import '../lib/makeswift/register-components'
+
+export {
+  getStaticPaths,
+} from '@makeswift/runtime/next'
+
+import { getStaticProps as getStaticMakeswiftProps, Page, PageProps } from '@makeswift/runtime/next';
+import { GetStaticPropsContext, PreviewData } from 'next';
+import { SWRConfig } from 'swr';
+import { get } from 'lodash';
+
+const fetchFromEndpoint = (_key: string) => {
+
+  return Promise.resolve(
+    {
+        "abc": "def",
+        "def": [
+          1, "2"
+        ]
+    }
+  )
+
+}
+
+
+const fetcher = async (key: string, sep: string = "$$") => {
+  const [endpoint, objectKey] = key.split(sep);
+  const endpointData = await fetchFromEndpoint(endpoint);
+  return objectKey ? get(endpointData, objectKey) : endpointData;
+}
+
+type DataMapping = {
+  [key: string]: string[];
+}
+
+const preloaded: DataMapping = {
+  
+  '/': [
+    '/data'
+  ],
+  '/protocols': []
+}
+
+type Data = {
+  [key: string]: any;
+}
+
+const destructure = (object: any, currentPath: string, data: Data = {}) => {
+
+  if (Array.isArray(object)) {
+    object.forEach((v, i) => {
+      destructure(v, `${currentPath}[${i}]`, data); 
+    });
+  } else if (typeof object === 'object') {
+    Object.entries(object).forEach(([k, v]) => {
+      if (currentPath.endsWith("$$")) {
+        destructure(v, `${currentPath}${k}`, data);
+      } else {
+        destructure(v, `${currentPath}.${k}`, data);
+
+      }
+    })
+  } else {
+    data[currentPath] = object; 
+  }
+
+  return data;
+
+}
+
+
+export const getStaticProps = async (props: GetStaticPropsContext<{ path: string[]; }, PreviewData>) => {
+  const makeswiftResults = await getStaticMakeswiftProps(props);
+  const fullEndpoint = props?.params?.path?.join("/") || "/";
+  const relevantDataKeysForPage = preloaded[fullEndpoint] ?? [];
+  const data: {
+    [key: string]: any
+  } = {};
+  for (const key of relevantDataKeysForPage) {
+    data[key] = await fetcher(key);
+  }
+
+  return { ...makeswiftResults, props: {
+      // @ts-ignore
+     ...makeswiftResults?.props,
+     fallback: Object.entries(data).reduce((acc, [key, value]) => ({
+      ...acc,
+      ...destructure(value, `${key}$$`, {}),
+      [key]: value,
+     }), {})
+     
+    } 
+  }  
+}
+
+type Fallback = {
+  [key: string]: any;
+}
+
+
+const IndexPage = ({fallback, ...props}: PageProps & Fallback) => {
+  return <SWRConfig value={{ fallback, fetcher }}><Page {...props} /></SWRConfig>
+}
+
+export default IndexPage;
